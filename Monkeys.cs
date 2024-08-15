900b1cf032f8ee0f03ead8c3bf4c9af0fcaa09fc
e.lyudmer <e.lyudmer@ctm.ru>
2024-08-15 11:49:29 +0300

    visionно лишенее

--- Monkeys.cs
+++ Monkeys.cs
@@ -12,10 +12,10 @@ namespace DZ_PaternPrototip2
             public Monkeys(float weight,  string mysound, string color, string habitat, string name, int age)
                 : base(color, habitat, name, age)
             {
+                
                 MyWeight = weight;
                 MySound = mysound;
             }
-            public float MySize { get; set; }
             public float MyWeight { get; set; }
             public string MySound { get; set; }
             public override string ToString()
